85a9fa4f8a9b053e4f833995cc70e3d792f4fdb6
Cong Lyu <15602214230@163.com>
2025-07-27 19:52:14 +0930

    Some general changes on App.jsx and index.html.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import { useState } from 'react'
 import './App.css'
 import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom'
-import {Layout} from './pages/userpages/layout.jsx'
-const router = createBrowserRouter(createRoutesFromElements(
-  <Route path='/' element={<Layout />}>
-    <Route path='/login' element={<Login />}/>
-  </Route>
-)) 
+import { Header, Loader } from './pages/userpages/Header.jsx'
+import { Login } from './pages/userpages/login.jsx'
+import { Vip } from './pages/userpages/Vip.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const router = createBrowserRouter(createRoutesFromElements(
+    <>
+    <Route path='/' element={<Header />}>
+      <Route path='/vip' loader={Loader} element={<Vip />} />
+    </Route>
+    <Route path='/login' element={<Login />}/>
+    </>
+  )) 
 
   return (
     <RouterProvider router={router}/>
