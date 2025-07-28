25c206394123fc908c5355f0aeafd8d22c33ae66
Cong Lyu <15602214230@163.com>
2025-07-28 19:51:09 +0930

    Create the login page and style it. Create a fetch request to send sign up token from front end to the target email address by the action attribute in Route.

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom'
 import { Header, Loader } from './pages/userpages/Header.jsx'
-import { Login } from './pages/userpages/login.jsx'
+import { Login, action } from './pages/userpages/login.jsx'
 import { Vip } from './pages/userpages/Vip.jsx'
 
 function App() {
@@ -11,7 +11,7 @@ function App() {
     <Route path='/' element={<Header />}>
       <Route path='/vip' loader={Loader} element={<Vip />} />
     </Route>
-    <Route path='/login' element={<Login />}/>
+    <Route action={action} path='/login' element={<Login />}/>
     </>
   )) 
 
--- src/pages/userpages/login.jsx
+++ src/pages/userpages/login.jsx
@@ -1,63 +1,88 @@
 import { Form, Link } from "react-router-dom"
 import styles from './login.module.css'
+import { useState } from "react"
+
+async function sendToken(email) {
+  const sendEmail = await fetch('http://localhost:5000/api/tokenGenerator', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      'email': email
+    })
+  })
+  const result = await sendEmail.text()
+}
+
+export async function action({request}) {
+  const event = await request.formData()
+  const buttonType = event.get('button')
+  if(buttonType === 'tokenFunction') {
+    sendToken(event.get('email'))
+  }
+}
 
 export function Login() {
+  const [logInOption, setLogInOption] = useState(true)
+
   return (
     <>
     <div className={styles.container}>
       <div className={styles.secondContainer}>
         <div className={styles.leftContainer}>
-          <p className={styles.qrPrompt1}>
-            扫描二维码登录
+          <p style={{marginBottom: '20px'}} className={styles.qrPrompt1}>
+            login by QR code
           </p>
           <div className={styles.qrContainer}>
             <img className={styles.qrCodeImg} src="/pictures/app-pictures/loginQR.jpg" />
-            <p className={styles.qrPrompt2}>
-              请使用<span>客户端</span>扫码登录或扫码下载APP
+            <p style={{fontSize: '14px', marginTop: '20px'}} className={styles.qrPrompt2}>
+              Please scan with <span>your device</span> to login or download the app
             </p>
           </div>
         </div>
-        <div className={styles.rightFormContainer}>
-          <div>
-            <Link className={styles.link}>
-              密码登录
-            </Link>
-            <Link className={styles.link}>
-              短信登录
-            </Link>
+        <div className={styles.rightContainer}>
+          <div className={styles.loginOptionContainer1}>
+            <button onClick={() => {setLogInOption(true)}} style={{cursor: logInOption ? 'not-allowed' : 'pointer', color: logInOption ? 'blue' : 'black'}} className={styles.logInOption}>
+              login with password
+            </button>
+            <button onClick={() => {setLogInOption(false)}} style={{cursor: logInOption ? 'pointer' : 'not-allowed', color: logInOption ? 'black' : 'blue'}} className={styles.logInOption}>
+              login with Email token
+            </button>
           </div>
           <Form className={styles.formContainer}>
             <div style={{borderBottom: 'none', borderBottomRightRadius: '0', borderBottomLeftRadius: '0'}} className={styles.inputContainer}>
-              <p className={styles.inputPrompt}>账号</p>
-              <input className={styles.input} placeholder="请输入账号"/>
+              <p className={styles.inputPrompt}>{logInOption ? 'username' : 'email'}</p>
+              <input className={styles.input} name='email' placeholder={logInOption ? 'your email address' : 'your email'}/>
             </div>
-            <div style={{borderTopRightRadius: '0', borderTopLeftRadius: '0'}} className={styles.inputContainer}>
-              <p className={styles.inputPrompt}>密码</p>
-              <input type="password" className={styles.input} placeholder="请输入密码"/>
+            <div style={{borderTopRightRadius: '0', borderTopLeftRadius: '0', marginBottom: '20px'}} className={styles.inputContainer}>
+              <p className={styles.inputPrompt}>{logInOption ? 'password' : 'token'}</p>
+              <input type={logInOption ? 'password' : 'text'} name={logInOption ? 'password' : 'tokenInput'} className={styles.input} placeholder={logInOption ? 'password here' : 'check your inbox for an one-time token'}/>
+              {logInOption || <button formMethod="POST" type="submit" name='button' value='tokenFunction' className={styles.tokenButton}>get token</button>}
             </div>
-            <div>
-              <button>注册</button>
-              <button>登录</button>
+            <div style={{marginBottom: '20px'}} className={styles.formButtonsContainer}>
+              <button type="button" onClick={() => {setLogInOption(false)}} className={styles.signUp}>sign up</button>
+              {logInOption && <button type="submit" className={styles.logIn}>go!</button>}
             </div>
           </Form>
-          <div>
-            其他方式登录
+          <div style={{marginBottom: '16px'}}>
+            login with other options
           </div>
-          <div>
+          <div className={styles.loginOptionContainer2}>
             <Link className={styles.link}>
-              微信登录
+              login with google
             </Link>
             <Link className={styles.link}>
-              微博登录
+              login with facebook
             </Link>
             <Link className={styles.link}>
-              QQ登录
+              login with xxx
             </Link>
           </div>
         </div>
       </div>
       <div className={styles.policy}>
-        未注册过的手机号，我们将自动帮您注册账号。登录或完成注册即代表您同意<span>用户协议</span>和<span>隐私政策</span>
+        By logging in, you agree with our <span className={styles.userPolicy}>user policy</span> and <span className={styles.userPolicy}>privacy policy</span>
       </div>
     </div>
     </>
