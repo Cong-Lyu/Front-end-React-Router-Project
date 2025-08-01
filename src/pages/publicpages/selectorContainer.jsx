b60b29bba051839c2e7f806c3333006cc5d4fbf4
Cong Lyu <lyu0061@flinders.edu.au>
2025-08-01 15:09:45 +0930

    Make selector container component.

---
+++ src/pages/publicpages/selectorContainer.jsx
@@ -0,0 +1,32 @@
+import styles from './selectorContainer.module.css'
+
+export function SelectorContainer() {
+  return (
+    <>
+      <div className={styles.selectorContainer}>
+        <p className={styles.option}>series</p>
+        <p className={styles.option}>domestic</p>
+        <p className={styles.option}>domestic</p>
+        <p className={styles.option}>domestic</p>
+        <p className={styles.option}>domestic</p>
+        <p className={styles.option}>domestic</p>
+        <p className={styles.option}>domestic</p>
+        <p className={styles.option}>domestic</p>
+        <p className={styles.option}>domestic</p>
+        <p className={styles.option}>domestic</p>
+        <p className={styles.option}>domestic</p>
+        <p className={styles.option}>domestic</p>
+        <p className={styles.option}>domestic</p>
+        <p className={styles.option}>domestic</p>
+        <p className={styles.option}>domestic</p>
+        <p className={styles.option}>domestic</p>
+        <p className={styles.option}>domestic</p>
+        <p className={styles.option}>domestic</p>
+        <p className={styles.option}>domestic</p>
+        <p className={styles.option}>domestic</p>
+        <p className={styles.option}>domestic</p>
+        <p className={styles.option}>domestic</p>
+      </div>
+    </>
+  )
+}
