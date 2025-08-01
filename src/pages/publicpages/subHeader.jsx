3fc19d303d409ae5d3105cfcfaef5a2260d0e41c
Cong Lyu <lyu0061@flinders.edu.au>
2025-08-01 15:11:44 +0930

    Create a subHeader component for the :type Route.

---
+++ src/pages/publicpages/subHeader.jsx
@@ -0,0 +1,58 @@
+import { Link, Outlet } from 'react-router-dom'
+import { useRef, useState, useEffect } from 'react'
+import { 
+  FireIcon,
+  BarsArrowUpIcon,
+  NewspaperIcon,
+  FlagIcon,
+  ChatBubbleLeftRightIcon,
+  PlayCircleIcon,
+  BookOpenIcon,
+  MusicalNoteIcon
+} from '@heroicons/react/24/solid'
+import styles from './subHeader.module.css'
+import { SelectorContainer } from './selectorContainer'
+
+export function SubHeader() {
+  const [selectorNum, setSelectorNum] = useState(22)
+  const winWidth = useRef(window.innerWidth)
+  console.log(winWidth)
+  useEffect(() => {
+    function handleResize() {
+      if(window.innerWidth < 1366) {
+        
+      }
+    }
+
+    window.addEventListener('resize', handleResize)
+
+    return window.removeEventListener('resize', handleResize)
+  }, [])
+
+  return (
+    <>
+      <div className={styles.container}>
+        <div className={styles.leftContainer}>
+          <div className={styles.leftBox}>
+            <BarsArrowUpIcon className={styles.leftIcon1}/>
+            <p className={styles.leftPrompt}>live updates</p>
+          </div>
+          <div className={styles.leftBox}>
+            <FireIcon className={styles.leftIcon2}/>
+            <p className={styles.leftPrompt}>hot videos</p>
+          </div>
+        </div>
+        <SelectorContainer selectorNum = {selectorNum}/>
+        <div className={styles.rightContainer}>
+          <div className={styles.rightSubContainer}><NewspaperIcon className={styles.rightIcons}/><p>column</p></div>
+          <div className={styles.rightSubContainer}><FlagIcon className={styles.rightIcons}/><p>activities</p></div>
+          <div className={styles.rightSubContainer}><ChatBubbleLeftRightIcon className={styles.rightIcons}/><p>community</p></div>
+          <div className={styles.rightSubContainer}><PlayCircleIcon className={styles.rightIcons}/><p>streams</p></div>
+          <div className={styles.rightSubContainer}><BookOpenIcon className={styles.rightIcons}/><p>tutorials</p></div>
+          <div className={styles.rightSubContainer}><MusicalNoteIcon className={styles.rightIcons}/><p>hot songs</p></div>
+        </div>
+      </div>
+      <Outlet />
+    </>
+  )
+}
