43e3498fd4c73871953ae902860e82787b936b99
Cong Lyu <lyu0061@flinders.edu.au>
2025-08-01 15:13:08 +0930

    Create :type Route and its parent Route.

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,17 @@ import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } 
 import { Header, Loader as HeaderLoader} from './pages/userpages/Header.jsx'
 import { Login, action } from './pages/userpages/login.jsx'
 import { Vip, Loader as VipLoader } from './pages/userpages/Vip.jsx'
+import { SubHeader } from './pages/publicpages/subHeader.jsx'
+import { Videos } from './pages/publicpages/Videos.jsx'
 
 function App() {
   const router = createBrowserRouter(createRoutesFromElements(
     <>
     <Route path='/' element={<Header />}>
+      <Route element={<SubHeader />}>
+        <Route path=':type' element={<Videos />}/>
+      </Route>
+
       <Route path='vip' loader={VipLoader} element={<Vip />} />
     </Route>
     <Route action={action} path='/login' element={<Login />}/>
