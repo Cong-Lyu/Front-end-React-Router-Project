b5929be7da9eb743fbf8f9e6bf8ad276999ee60b
Cong Lyu <lyu0061@flinders.edu.au>
2025-09-06 18:03:59 +0930

    Create googleOAuth onSuccess and onError functions.

---
+++ src/util/googleLogInEventHandlers.js
@@ -0,0 +1,18 @@
+export async function onSuccess(response) {
+  const url = import.meta.env.VITE_REACT_APP_API_URL || `http://localhost:5000`
+  const request = await fetch(`${url}/api/jwt/logIn`, {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json'
+    }, 
+    body: JSON.stringify({token: response.credential})
+  })
+  const result = await request.json()
+  localStorage.setItem('googleJwt', result['googleJwt'])
+  localStorage.setItem('myJwt', result['myJwt'])
+  console.log(result)
+}
+
+export function onError() {
+  alert('Log in failed!')
+}
