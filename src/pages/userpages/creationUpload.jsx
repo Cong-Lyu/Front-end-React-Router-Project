153daf3e78cd6a0b78a40c829d0aa8005ea24486
Cong Lyu <lyu0061@flinders.edu.au>
2025-09-24 14:51:26 +0930

    Renew the upload route's elements.

--- src/pages/userpages/creationUpload.jsx
+++ src/pages/userpages/creationUpload.jsx
@@ -1,101 +1,60 @@
 import styles from './creationUpload.module.css'
-import { Form, useActionData, redirect } from 'react-router-dom'
-import { useRef, useState } from 'react'
-
-function splitFileName(fullName) {
-  const parts = fullName.split('.')
-  const extension = parts.pop() 
-  const name = parts.join('.');         
-  return [name, extension]
-}
-
-export async function loader() {
-  const token = JSON.parse(localStorage.getItem('latestUser') || '{}')['user-token']
-  if(token) {
-    const varify = await fetch('http://localhost:5000/api/token/tokenVarify', {
-      method: 'POST',
-      headers: {
-        'Content-type': 'application/json'
-      },
-      body: JSON.stringify({
-        'user-token': token
-      })
-    })
-    const result = await varify.json()
-    console.log('This is the token varifying:', result)
-    if(!result['token-status']) {
-      throw redirect('/login')
-    }
-  }
-  else {
-    throw redirect('/login')
-  }
-}
-
-export async function action({ request }) {
-  const formData = await request.formData()
-  const type = formData.get('uploadImage')
-  const image = formData.get('image')
-  const imageName = splitFileName(image)
-  const name = imageName[0]
-  const extension = imageName[1]
-  const token = JSON.parse(localStorage.getItem('latestUser'))['user-token']
-  let url = ``
-  if(type === 'big') {
-    url = 'http://localhost:5000/api/public/' + 'big'
-  }
-  else {
-    url = 'http://localhost:5000/api/public/' + 'general'
-  }
-  const sendPictureName = await fetch(url, {
-    method: 'POST',
-    headers: {
-      'Content-type': 'application/json',
-      'user-token': token
-    },
-    body: JSON.stringify({
-      'pictureName': name,
-      'pictureExtension': extension
-    })
-  })
-  const result = await sendPictureName.json()
-  return result['insertPictureStatus']
-}
+import { useState } from 'react'
 
 export function CreationUpload() {
-  const imageInput = useRef(null)
-  const [hasUploaded, setHasUploaded] = useState(false)
-  const uploadStatus = useActionData()
-
-  if(uploadStatus === true) {
-    console.log('Uploaded successfully!')
-    window.location.reload();
-  }
-  else if(uploadStatus === false) {
-    console.log('Upload failed, please try again later...')
-    window.location.reload();
-  }
-
-  function getImage() {
-    imageInput.current.click() 
-  }
-
-  function handleImage() {
-    setHasUploaded(true)
-  }
+  
 
   return (
     <>
-      <div className={styles.main}>
-        <div className={styles.mainContent}>
-          <Form method='post'>
-            <input name='image' onChange={handleImage} ref={imageInput} style={{display: 'none'}} type='file' accept="image/*"/>
-            <button type='button' onClick={hasUploaded ? undefined : getImage}>choose your image here</button>
-            <button type='submit' name='uploadImage' value='big'>upload it as a big image!</button>
-            <button type='submit' name='uploadImage' value='small'>upload it as a small image!</button>
-          </Form>
-        </div>
-      </div>
+      <UploadElems />
+    </>
+  )
+}
+
+function UploadElems() {
+  const [videoName, setVideoName] = useState(null)
+  const [coverName, setCoverName] = useState(null)
+
+  return (
+    <>
+      <main className={styles.main} >
+        <label className={videoName ? styles.uploadContainerDisabled : styles.uploadContainer} >
+          {videoName ? 
+            <div className={styles.instance} >
+              {videoName}
+              <button className={styles.deleteInstance} onClick={(e) => {e.stopPropagation(); e.preventDefault(); setVideoName(null)}} >
+                ×
+              </button>
+            </div> : "Upload your video here"
+          }
+          <input 
+            key={videoName || 'empty'} 
+            disabled={videoName} 
+            type='file' 
+            accept='video/*' 
+            onChange={(e) => {if (e.target.files.length > 0) {setVideoName(e.target.files[0].name)}}} 
+            style={{display: "none"}} />
+        </label>
+        <button disabled={!videoName} className={styles.uploadButton} >Upload your video</button>
+        <label className={coverName ? styles.uploadContainerDisabled : styles.uploadContainer} >
+          {coverName ? 
+            <div className={styles.instance} >
+              {coverName}
+              <button className={styles.deleteInstance} onClick={(e) => {e.stopPropagation(); e.preventDefault(); setCoverName(null)}} >
+                ×
+              </button>
+            </div> : "Upload your video cover image here"
+          }
+          <input
+            key={coverName || 'empty'} 
+            disabled={coverName}
+            type='file' 
+            accept='image/*' 
+            onChange={(e) => {if (e.target.files.length > 0) {setCoverName(e.target.files[0].name)}}} 
+            style={{display: "none"}}/>
+        </label>
+        <button disabled={!coverName} className={styles.uploadButton} >Upload a cover image</button>
+      </main>
     </>
   )
 }
