5aa53be2379a491cd3b0e1d81c0cdcdb40e79c50
Cong Lyu <lyu0061@flinders.edu.au>
2025-09-24 14:45:43 +0930

    Remove the old upload route and add a new route.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
-import { useState } from 'react'
 import './App.css'
 import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom'
 import { Header } from './pages/userpages/Header.jsx'
 import { Premium } from './pages/publicpages/Premium.jsx'
 import { SubHeader } from './pages/publicpages/subHeader.jsx'
 import { Videos, loader as videoLoader } from './pages/publicpages/Videos.jsx'
-import { CreationLeftBar } from './pages/userpages/creationLeftBar.jsx'
-import { CreationUpload, action as uploadAction, loader as creationLoader } from './pages/userpages/creationUpload.jsx'
+import { CreationUpload } from './pages/userpages/creationUpload.jsx'
 import { VideoPlaying } from './pages/publicpages/VideoPlaying.jsx'
 import { PremiumPayment, Loader as paymentLoader } from './pages/publicpages/PremiumPayment.jsx'
 import { PremiumVideos } from './pages/publicpages/PremiumVideos.jsx'
@@ -28,15 +26,13 @@ function App() {
 
       <Route loader={userPageLoader} element={<UserSubHeader />}>
         <Route path='userPage' element={<UserPage />} />
+        <Route path='upload' element={<CreationUpload />} /> 
       </Route>
       <Route path='premiumVideoPlaying' loader={premiumVideoLoader} element={<PremiumVideoPlaying />} />
       <Route path='premium' loader={paymentLoader} element={<Premium />} />
       <Route path='premiumVideos' loader={videoLoader} element={<PremiumVideos />} />
     </Route>
     <Route path='premiumPayment' loader={paymentLoader} element={<PremiumPayment />}/>
-    <Route element={<CreationLeftBar />}>
-      <Route loader={creationLoader} action={uploadAction} path='/creation' element={<CreationUpload />} /> 
-    </Route>
     </>
   )) 
 
