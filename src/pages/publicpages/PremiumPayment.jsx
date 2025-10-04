0d27b06f2e8e4adce6e00e2e81c5c7323f675ea4
Cong Lyu <lyu0061@flinders.edu.au>
2025-10-04 14:02:02 +0930

    Show error

--- src/pages/publicpages/PremiumPayment.jsx
+++ src/pages/publicpages/PremiumPayment.jsx
@@ -20,7 +20,7 @@ export async function Loader() {
     })
     const result = await varify.json()
     console.log(result)
-    if(!result['status']) {return false}
+    if(!result['status']) {console.log(result['error']); return false}
     return true
   }
   catch(err) {
